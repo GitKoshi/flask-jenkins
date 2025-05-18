982de3ee72db57db833115f491b2d2d58cc6c218
GitKoshi <fasoletievan@gmail.com>
2025-05-18 21:06:37 +0200

    Tung Tung Tung Sahuuur

--- appEvan.py
+++ appEvan.py
@@ -1,10 +1,9 @@
 from flask import Flask
-
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Heureusement que chatgpt existe ^^"
+    return "Tung Tung Tung Sahuuurrr"
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000)
